6843d24d441b5a85cbeb17541c73371485387aaa
candescent-mouse-0917 <aqiph818@gmail.com>
2020-02-11 04:43:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+//getFirstSelector(selector)
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+//nestedTarget()
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+//increaseRankBy(n)
+function increaseRankBy(n){
+  let rankList = document.querySelectorAll('.ranked-list li');
+  for (let i = 0; i<rankList.length;i++){
+    rankList[i].innerHTML = parseInt(rankList[i].innerHTML) + n;
+  }
+}
+
 
